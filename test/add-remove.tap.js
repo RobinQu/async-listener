29681eb83db6ecd28361d3c71b5c48b310335366
RobinQu <long.qul@alibaba-inc.com>
2016-08-26 21:20:39 +0800

    save

--- test/add-remove.tap.js
+++ test/add-remove.tap.js
@@ -1,39 +1,43 @@
 'use strict';
 
-var test = require('tap').test;
+const test = require('tap').test;
 
-test("async listener lifecycle", function (t) {
+test('async listener lifecycle', function (t) {
   t.plan(8);
 
-  if (!process.addAsyncListener) require('../index.js');
+  if (!process.addAsyncListener) {
+    require('../index.js');
+  }
 
-  t.ok(process.createAsyncListener, "can create async listeners");
-  var counted = 0;
-  var listener = process.createAsyncListener(
+  t.ok(process.createAsyncListener, 'can create async listeners');
+  let counted = 0;
+  let listener = process.createAsyncListener(
     {
-      create : function () { counted++; },
-      before : function () {},
-      after  : function () {},
-      error  : function () {}
+      create: function () {
+        counted++;
+      },
+      before: function () {},
+      after: function () {},
+      error: function () {}
     },
     Object.create(null)
   );
 
-  t.ok(process.addAsyncListener, "can add async listeners");
+  t.ok(process.addAsyncListener, 'can add async listeners');
   t.doesNotThrow(function () {
     listener = process.addAsyncListener(listener);
-  }, "adding does not throw");
+  }, 'adding does not throw');
 
-  t.ok(listener, "have a listener we can later remove");
+  t.ok(listener, 'have a listener we can later remove');
 
-  t.ok(process.removeAsyncListener, "can remove async listeners");
+  t.ok(process.removeAsyncListener, 'can remove async listeners');
   t.doesNotThrow(function () {
     process.removeAsyncListener(listener);
-  }, "removing does not throw");
+  }, 'removing does not throw');
 
   t.doesNotThrow(function () {
     process.removeAsyncListener(listener);
-  }, "failing remove does not throw");
+  }, 'failing remove does not throw');
 
-  t.equal(counted, 0, "didn't hit any async functions");
+  t.equal(counted, 0, 'didn\'t hit any async functions');
 });
